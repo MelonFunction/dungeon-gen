a208b2a8bd1d584f4ff5812afd2cad5fe532b45f
MelonFunction <8727952+MelonFunction@users.noreply.github.com>
2022-08-23 17:53:00 +0100

    initial commit

---
+++ generate.go
@@ -0,0 +1,2 @@
+// Package generate provides types and functions to generate different dungeons and environments
+package generate
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/melonfunction/dungeon-gen
+
+go 1.19
