a7ee5b9b682308ea92f821038ad7204affd83294
MelonFunction <8727952+MelonFunction@users.noreply.github.com>
2022-08-29 18:11:12 +0100

    improve example

--- examples/main.go
+++ examples/main.go
@@ -8,34 +8,54 @@ import (
 	gen "github.com/melonfunction/dungeon-gen"
 )
 
+// Generation styles
+const (
+	RandomWalk int = iota
+	DungeonGrid
+	Dungeon
+)
+
 func main() {
 	log.SetFlags(log.Lshortfile)
 
-	w, h := 80, 80
+	w, h := 80, 80 // default terminal width (hopefully)
 	world := gen.NewWorld(w, h)
 	world.Border = 2
-	world.WallThickness = 2
 	world.CorridorSize = 2
 	world.MaxRoomWidth = 8
 	world.MaxRoomHeight = 8
 	world.MinRoomWidth = 4
 	world.MinRoomHeight = 4
-	world.AllowRandomCorridorOffset = true
 
-	err := world.GenerateRandomWalk((80 * 80) / 4)
-	// err := world.GenerateDungeonGrid(5 * 5)
-	// err := world.GenerateDungeon(30)
+	style := Dungeon
+	var err error
+	switch style {
+	case RandomWalk:
+		world.WallThickness = 2
+		world.MinIslandSize = 26 // 26 is default
+		err = world.GenerateRandomWalk((80 * 80) / 4)
+		world.CleanIslands()
+		world.CleanWalls(5)
+		world.CleanWalls(5)
+		world.CleanIslands()
+		world.CleanWalls(6)
+		world.CleanWalls(6)
+		world.AddWalls()
+	case DungeonGrid:
+		world.WallThickness = 2
+		world.AllowRandomCorridorOffset = false
+		err = world.GenerateDungeonGrid(5 * 5)
+		world.AddWalls()
+	case Dungeon:
+		world.WallThickness = 1
+		world.AllowRandomCorridorOffset = true
+		err = world.GenerateDungeon(30)
+		world.AddWalls()
+	}
+
 	if err != nil {
 		log.Println(err)
 	}
-
-	world.CleanIslands()
-	world.CleanWalls(5)
-	world.CleanWalls(5)
-	world.CleanIslands()
-	world.CleanWalls(6)
-	world.CleanWalls(6)
-	world.AddWalls()
 
 	for y := 0; y < h; y++ {
 		for x := 0; x < w; x++ {
